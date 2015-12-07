942ec4a4f41b0041aa31689ca3c27602f8b2fbe8
blakewallan <blakewallan@gmail.com>
2015-12-06 18:00:03 -0800

    created conversion funciton

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var db = require('./models');
+var Hashids = require('hashids'),
+    hashids = new Hashids('this is my salt');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/static'));
+
+//Render home
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+
+
+//Convert link to hash id
+var numConvert = function(link){
+    var converted = "";
+    for(var i = 0; i < link.length; i ++){
+        converted += link.charCodeAt(i);
+    }
+    return hashids.encode(parseInt(converted));
+}
+
+
+app.listen(3000);
