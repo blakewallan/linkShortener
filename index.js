f3e54088cd2a5aa97cf9f27b0f30d2040a8fa12f
blakewallan <blakewallan@gmail.com>
2015-12-06 21:25:55 -0800

    core functionality complete

--- index.js
+++ index.js
@@ -11,21 +11,42 @@ app.use(express.static(__dirname + '/static'));
 
 //Render home
 app.get('/', function(req, res){
-    res.render('index');
+    db.link.findAll().then(function(links){
+        res.render('index', {links : links});
+    });
+    //res.render('index');
 });
 
+app.post('/', function(req, res){
+    var newLink = {
+        original : req.body.og,
+        new : numConvert(req.body.og),
+        clicks : 0
+    };
+    db.link.create(newLink).then(function(){
+        res.render('show', {links : newLink});
+    });
+});
 
+app.get('/:id', function(req, res){
+    var index = req.params.id;
+    console.log("localhost:3000/" + index);
+    var thing = db.link.findAll({
+        where : {
+            new : "localhost:3000/" + String(index)
+        }
+    }).then(function(links){
+        res.redirect(links[0].dataValues.original);
+    });
+});
 
 //Convert link to hash id
 var numConvert = function(link){
-    var converted = "localhost:3000/";
+    var converted = "";
     for(var i = 0; i < link.length; i ++){
         converted += link.charCodeAt(i);
     }
-    return hashids.encode(parseInt(converted));
+    return ("localhost:3000/" + String(hashids.encode(parseInt(converted))));
 }
-
-console.log(numConvert("alsjdflajfdlasasdfasfads"));
-
 
 app.listen(3000);
